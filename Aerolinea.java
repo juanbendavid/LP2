701ec364ca2409b9e2a5005b12f81b3aebfdca22
Juan David <juanemanueldavidzaracho@gmail.com>
2022-09-28 22:32:01 -0400

    creacion de la clase vuelo

--- Aerolinea.java
+++ Aerolinea.java
@@ -1,4 +1,4 @@
-public class Aerolinea{
+public class Aerolinea {
 
     public static void main(String[] args) {
         
---
+++ Vuelo.java
@@ -0,0 +1,7 @@
+public class Vuelo {
+    int []asientos;
+    
+    public Vuelo(){
+        
+    }
+}
